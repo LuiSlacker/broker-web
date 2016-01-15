a4e330b88a2da3184fd36de2b442a9759248f574
Petulant Slacker <l.goohsen@gmail.com>
2016-01-15 16:48:33 +0100

    inserted output - Element

--- WEB-INF/js/closed-auctions-controller.js
+++ WEB-INF/js/closed-auctions-controller.js
@@ -46,24 +46,23 @@ this.de.sb.broker = this.de.sb.broker || {};
 	de.sb.broker.ClosedAuctionsController.prototype.displayClosedAuctions = function () {
 		var self = this;
 		var user = this.sessionContext.user;
-		de.sb.util.AJAX.invoke("/services/people/" + user.identity + "/auctions?closed=true", "GET", {"Accept": "application/json"}, null, user, function (request) {
-			self.displayStatus(request.status, request.statusText);
+		de.sb.util.AJAX.invoke("/services/people/" + user.identity + "/auctions?closed=true&seller=true", "GET", {"Accept": "application/json"}, null, user, function (request) {
+			self.statusLog.push({"status": request.status, "statusText": request.statusText});
 			if (request.status === 200) {
 				var auctions = JSON.parse(request.responseText);
 				auctions.forEach(function(auction, index){
-					if (auction.sellerReference == user.identity){
-						var winningBid = de.sb.broker.ClosedAuctionsController.prototype.findAuctionWinningBid(auction);
-						var tableRowElement = document.querySelector("#auction-table-row").content.querySelector('tr').cloneNode(true);
-						var tableCells = tableRowElement.querySelectorAll('td');
-						tableCells[0].innerHTML = (winningBid) ? winningBid.bidder.alias : "no bidder";
-						tableCells[1].innerHTML = self.prettyDate(auction.creationTimestamp);
-						tableCells[2].innerHTML = self.prettyDate(auction.closureTimestamp);
-						tableCells[3].innerHTML = auction.title;
-						tableCells[4].innerHTML = auction.unitCount;
-						tableCells[5].innerHTML = de.sb.broker.ClosedAuctionsController.prototype.prettyPrice(auction.askingPrice);
-						tableCells[6].innerHTML = (winningBid) ? de.sb.broker.ClosedAuctionsController.prototype.prettyPrice(winningBid.price) : "-";
-						document.querySelector("section.closed-seller-auctions tbody").appendChild(tableRowElement);
-					}
+					var winningBid = self.findAuctionWinningBid(auction);
+					var tableRowElement = document.querySelector("#auction-table-row").content.querySelector('tr').cloneNode(true);
+					var tableCells = tableRowElement.querySelectorAll('output');
+					tableCells[0].value = (winningBid) ? winningBid.bidder.alias : "no bidder";
+					tableCells[0].title = (winningBid) ? winningBid.bidder.name.given + " " + winningBid.bidder.name.family + " (" + winningBid.bidder.contact.email + ")": "";
+					tableCells[1].value = self.prettyDate(auction.creationTimestamp);
+					tableCells[2].value = self.prettyDate(auction.closureTimestamp);
+					tableCells[3].value = auction.title;
+					tableCells[4].value = auction.unitCount;
+					tableCells[5].value = de.sb.broker.ClosedAuctionsController.prototype.prettyPrice(auction.askingPrice);
+					tableCells[6].value = (winningBid) ? self.prettyPrice(winningBid.price) : "-";
+					document.querySelector("section.closed-seller-auctions tbody").appendChild(tableRowElement);
 				});
 			}
 		});	
@@ -95,7 +94,7 @@ this.de.sb.broker = this.de.sb.broker || {};
 				myBid = bid;
 			}
 		});
-		return myBid
+		return myBid;
 	}
 	
 	/**
@@ -104,44 +103,42 @@ this.de.sb.broker = this.de.sb.broker || {};
 	de.sb.broker.ClosedAuctionsController.prototype.displayBidsInClosedAuctions = function () {
 		var self = this;
 		var user = this.sessionContext.user;
-		de.sb.util.AJAX.invoke("/services/people/" + user.identity + "/auctions?closed=true", "GET", {"Accept": "application/json"}, null, user, function (request) {
+		de.sb.util.AJAX.invoke("/services/people/" + user.identity + "/auctions?closed=true&seller=false", "GET", {"Accept": "application/json"}, null, user, function (request) {
 			self.statusLog.push({"status": request.status, "statusText": request.statusText});
-			//self.displayStatus(request.status, request.statusText);
 			if (request.status === 200) {
 				var auctions = JSON.parse(request.responseText);
 				console.log(auctions);
 				auctions.forEach(function(auction, index){
-					if (auction.sellerReference != user.identity){
-						var winningBid = de.sb.broker.ClosedAuctionsController.prototype.findAuctionWinningBid(auction);
-						var seller = self.getPersonFromId(auction.sellerReference);
-						var tableRowElement = document.querySelector("#bid-table-row").content.querySelector('tr').cloneNode(true);
-						var tableCells = tableRowElement.querySelectorAll('td');
-						tableCells[0].innerHTML = "";
-						tableCells[1].innerHTML = (winningBid) ? winningBid.bidder.alias : "no bidder";
-						tableCells[2].innerHTML = self.prettyDate(auction.creationTimestamp);
-						tableCells[3].innerHTML = self.prettyDate(auction.closureTimestamp);
-						tableCells[4].innerHTML = auction.title;
-						tableCells[5].innerHTML = auction.unitCount;
-						tableCells[6].innerHTML = self.prettyPrice(auction.askingPrice);
-						tableCells[7].innerHTML = self.prettyPrice(self.findMyBidFromAuction(auction).price);
-						tableCells[8].innerHTML = (winningBid) ? self.prettyPrice(winningBid.price) : "-";
-						document.querySelector("section.closed-bidder-auctions tbody").appendChild(tableRowElement);
-					}
+					var winningBid = self.findAuctionWinningBid(auction);
+					var tableRowElement = document.querySelector("#bid-table-row").content.querySelector('tr').cloneNode(true);
+					var tableCells = tableRowElement.querySelectorAll('output');
+					tableCells[0].value = auction.seller.alias;
+					tableCells[0].title = auction.seller.name.given + " " + auction.seller.name.family + " (" + auction.seller.contact.email + ")";
+					tableCells[1].value = (winningBid) ? winningBid.bidder.alias : "no bidder";
+					//tableCells[1].title = (winningBid) ? winningBid.bidder.name.given + " " + winningBid.bidder.name.family + " (" + winningBid.bidder.contact.email + ")": "";
+					tableCells[2].value = self.prettyDate(auction.creationTimestamp);
+					tableCells[3].value = self.prettyDate(auction.closureTimestamp);
+					tableCells[4].value = auction.title;
+					tableCells[5].value = auction.unitCount;
+					tableCells[6].value = self.prettyPrice(auction.askingPrice);
+					tableCells[7].value = self.prettyPrice(self.findMyBidFromAuction(auction).price);
+					tableCells[8].value = (winningBid) ? self.prettyPrice(winningBid.price) : "-";
+					document.querySelector("section.closed-bidder-auctions tbody").appendChild(tableRowElement);
 				});
 			}
 		});	
 	}
-	
-	de.sb.broker.ClosedAuctionsController.prototype.getPersonFromId = function (id) {
-		var self = this;
-		var user = this.sessionContext.user;
-		de.sb.util.AJAX.invoke("/services/people/" + id, "GET", {"Accept": "application/json"}, null, user, function (request) {
-			self.statusLog.push({"status": request.status, "statusText": request.statusText});
-			var highestStatus = self.getHighestStatus(self.statusLog);
-			self.displayStatus(highestStatus.status, highestStatus.statusText);
-			if (request.status === 200) {
-				document.querySelectorAll("section.closed-bidder-auctions td")[0].innerHTML = JSON.parse(request.responseText).alias;
-			}
-		});
-	}
+//	
+//	de.sb.broker.ClosedAuctionsController.prototype.getPersonFromId = function (id) {
+//		var self = this;
+//		var user = this.sessionContext.user;
+//		de.sb.util.AJAX.invoke("/services/people/" + id, "GET", {"Accept": "application/json"}, null, user, function (request) {
+//			self.statusLog.push({"status": request.status, "statusText": request.statusText});
+//			var highestStatus = self.getHighestStatus(self.statusLog);
+//			self.displayStatus(highestStatus.status, highestStatus.statusText);
+//			if (request.status === 200) {
+//				document.querySelectorAll("section.closed-bidder-auctions td")[0].innerHTML = JSON.parse(request.responseText).alias;
+//			}
+//		});
+//	}
 } ());
